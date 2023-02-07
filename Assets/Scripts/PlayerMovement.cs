0c0f34c68696d99fe2e66ce27c59a35cb53090d9
Prime_N2 <nikitaprime83@gmail.com>
2023-02-07 15:47:02 +0400

    -add solution for third number in the corresponding scene

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D _playerBody;
+
+    [SerializeField] private float _thrust;
+
+	private void Awake()
+    {
+        _playerBody = GetComponent<Rigidbody2D>();
+	}
+
+    private void Start()
+    {
+        _playerBody.AddForce(transform.right * _thrust);
+    }
+}
