8b174478ca50a400ff1ca15d54c8ad49c2416f1f
Prime_N2 <nikitaprime83@gmail.com>
2023-02-14 17:30:57 +0400

    -add endless swinging

--- Assets/Scripts/PlayerJointsHandler.cs
+++ Assets/Scripts/PlayerJointsHandler.cs
@@ -1,19 +1,32 @@
 ﻿using System;
 using UnityEngine;
 
-[RequireComponent(typeof(Joint2D))]
+[RequireComponent(typeof(Joint2D), typeof(PlayerMovement))]
 public class PlayerJointsHandler : MonoBehaviour
 {
+	public Transform CurrentRope { get => _currentRope; }
+	[SerializeField] private Transform _currentRope;
+
+	private PlayerMovement _playerMovement;
 	private Joint2D _playerJoint;
 
 	private void Awake()
 	{
 		_playerJoint = GetComponent<Joint2D>();
+		_playerMovement = GetComponent<PlayerMovement>();
+	}
+
+	private void Start()
+	{
+		_playerMovement.StartSwinging(_currentRope.position);
 	}
 
 	public void UntieCurrentJoint()
 	{
+		_playerMovement.StopSwinging();
+
 		_playerJoint.connectedBody = null;
 		_playerJoint.enabled = false;
+		_currentRope = null;
 	}
 }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,19 +1,64 @@
+using System.Collections;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody2D))]
 public class PlayerMovement : MonoBehaviour
 {
-    private Rigidbody2D _playerBody;
+	[SerializeField] private float _swingVelocityThreshold;
+	[SerializeField] private float _swingForce;
+	[SerializeField] private float _initialThrust;
 
-    [SerializeField] private float _thrust;
+	private Rigidbody2D _playerRigidbody;
+	private Vector2 currentRopePosition;
+	private float _lastPositionX;
+	private bool _isSwinging = false;
 
 	private void Awake()
     {
-        _playerBody = GetComponent<Rigidbody2D>();
+        _playerRigidbody = GetComponent<Rigidbody2D>();
 	}
 
     private void Start()
     {
-        _playerBody.AddForce(transform.right * -1 * _thrust);
-    }
+		_playerRigidbody.AddForce(transform.right * _initialThrust);
+	}
+
+	public void StartSwinging(Vector2 ropePosition)
+	{
+		currentRopePosition = ropePosition;
+		_isSwinging = true;
+	}
+
+	public void StopSwinging()
+	{
+		_isSwinging = false;
+	}
+
+	private void FixedUpdate()
+	{
+		if (!_isSwinging)
+			return;
+
+		Vector2 playerToHookDirection = currentRopePosition - ((Vector2)transform.position).normalized;
+
+		if (_lastPositionX < currentRopePosition.x &&
+			transform.position.x > currentRopePosition.x &&
+			_playerRigidbody.velocity.x < _swingVelocityThreshold)
+		{
+			Vector2 perpendicularDirection = new Vector2(playerToHookDirection.y, -playerToHookDirection.x);
+			Vector2 rightPerpendicularPosition = (Vector2)transform.position + perpendicularDirection * 2f;
+
+			_playerRigidbody.AddForce(rightPerpendicularPosition * _swingForce);
+		}
+		else if (_lastPositionX > currentRopePosition.x &&
+			transform.position.x < currentRopePosition.x &&
+			_playerRigidbody.velocity.x > -_swingVelocityThreshold)
+		{
+			Vector2 perpendicularDirection = new Vector2(-playerToHookDirection.y, playerToHookDirection.x);
+			Vector2 leftPerpendicularPosition = (Vector2)transform.position - perpendicularDirection * -2f;
+
+			_playerRigidbody.AddForce(leftPerpendicularPosition * _swingForce);
+		}
+		_lastPositionX = transform.position.x;
+	}
 }
